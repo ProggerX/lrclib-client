c3a4e14036ac85a7233eb49eb89f069d52e48f97
ProggerX <x@proggers.ru>
2026-01-08 17:47:57 +0300

    test, fix: add decode types

--- test/Main.hs
+++ test/Main.hs
@@ -1,19 +1,24 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Main where
 
+import Control.Monad
 import Data.Aeson
 import Data.ByteString.Lazy as BS
+import Data.Kind
 import Data.Maybe
 import LrcLib.Client
 import Test.Tasty
 import Test.Tasty.HUnit
 
-decodeTest file = testCase file $ (fromJust . decode <$> BS.readFile file)
+decodeTest :: forall (a :: Type). (FromJSON a) => String -> TestTree
+decodeTest file = testCase file $ void (fromJust . decode @a <$> BS.readFile file)
 
 main :: IO ()
 main =
-  defaultMain . testGroup "decoding" . fmap decodeTest $
-    [ "test/challenge.json",
-      "test/getUrlSuccess.json",
-      "test/getUrlNotFound.json",
-      "test/search.json"
+  defaultMain . testGroup "decoding" $
+    [ decodeTest @Challenge "test/challenge.json",
+      decodeTest @GetResponse "test/getUrlSuccess.json",
+      decodeTest @GetResponse "test/getUrlNotFound.json",
+      decodeTest @SearchResponse "test/search.json"
     ]
